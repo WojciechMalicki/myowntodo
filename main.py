48b18c92d583b6842a4524c19ec6c168033e0379
Wojciech Malicki <wojciechmalicki@gmail.com>
2020-04-19 18:18:29 +0200

    fix smalls problems

--- main.py
+++ main.py
@@ -7,35 +7,37 @@ from PyQt5.QtWidgets import QMessageBox, QInputDialog
 from gui import Ui_Widget
 
 class Task(QWidget, Ui_Widget):
+	"""
+	application interface
+	"""
+	def __init__(self, parent=None):
+		super(Task, self).__init__(parent)
+		self.setupUi(self)
 
-    def __init__(self, parent=None):
-        super(Task, self).__init__(parent)
-        self.setupUi(self)
+		self.loginBtn.clicked.connect(self.login)
+		self.endBtn.clicked.connect(self.end)
 
-        self.loginBtn.clicked.connect(self.login)
-        self.endBtn.clicked.connect(self.end)
+	def login(self):
+		slogin, ok = QInputDialog.getText(self, 'Logowanie', 'Podaj login:')
+		if ok:
+			password, ok = QInputDialog.getText(self, 'Logowanie', 'Podaj haslo:')
+			if ok:
+				if not login or not password:
+					QMessageBox.warning(
+						self, 'Błąd', 'Pusty login lub hasło!', QMessageBox.Ok)
+					return
+				QMessageBox.information(
+					self, 'Dane logowania',
+					'Podano: ' + slogin + ' ' + password, QMessageBox.Ok)
 
-    def login(self):
-        slogin, ok = QInputDialog.getText(self, 'Logowanie', 'Podaj login:')
-        if ok:
-            password, ok = QInputDialog.getText(self, 'Logowanie', 'Podaj haslo:')
-            if ok:
-                if not login or not password:
-                    QMessageBox.warning(
-                        self, 'Błąd', 'Pusty login lub hasło!', QMessageBox.Ok)
-                    return
-                QMessageBox.information(
-                    self, 'Dane logowania',
-                    'Podano: ' + slogin + ' ' + password, QMessageBox.Ok)
-
-    def end(self):
-        self.close()
+	def end(self):
+		self.close()
 
 if __name__ == '__main__':
-    import sys
+	import sys
 
-    app = QApplication(sys.argv)
-    winapp = Task()
-    winapp.show()
-    winapp.move(350, 200)
-    sys.exit(app.exec_())
+	app = QApplication(sys.argv)
+	winapp = Task()
+	winapp.show()
+	winapp.move(350, 200)
+	sys.exit(app.exec_())
--- sqlraw.py
+++ sqlraw.py
@@ -1,1 +1,19 @@
-sqlraw.py
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+# create database connection
+class dbcon():
+
+	
+	def __init__(self, filename):
+		self.con = sqlite3.connect(filename)
+
+# access to column by index and by name
+con.row_factory = sqlite3.Row
+
+# create cursor object
+cur = con.cursor()
+
+# create table
